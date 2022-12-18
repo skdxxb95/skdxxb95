6f9863162aaf7a5db6c1a39491808c440c39a9c3
skdxxb95 <s3992963@student.rmit.edu.au>
2022-12-18 13:08:44 +1000

    Final touch ups before submission

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,12 +4,9 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='../../assets/Assignment1.pdf' download className='btn'>Download PDF</a>
         <a href='https://github.com/skdxxb95' target='_blank'  className='btn btn-primary'>GitHub</a>
     </div>
   )
 }
 
 export default CTA
-
-/* You need to link your final PDF. Import it and link it in the href */
